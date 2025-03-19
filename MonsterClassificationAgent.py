a9019a93f1f96fe68c6c7702d3c3ec00908f4b54
Zaid Hilweh <hilwehzaid@gmail.com>
2025-03-19 14:26:02 -0500

    inital commit

---
+++ MonsterClassificationAgent.py
@@ -0,0 +1,18 @@
+class MonsterClassificationAgent:
+    def __init__(self):
+        #If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, samples, new_monster):
+        #Add your code here!
+        #
+        #The first parameter to this method will be a labeled list of samples in the form of
+        #a list of 2-tuples. The first item in each 2-tuple will be a dictionary representing
+        #the parameters of a particular monster. The second item in each 2-tuple will be a
+        #boolean indicating whether this is an example of this species or not.
+        #
+        #The second parameter will be a dictionary representing a newly observed monster.
+        #
+        #Your function should return True or False as a guess as to whether or not this new
+        #monster is an instance of the same species as that represented by the list.
+        pass
